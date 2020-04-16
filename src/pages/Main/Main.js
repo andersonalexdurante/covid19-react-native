fa25ffadc0490286ae1c93c3273fe6ef0809fda7
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-04-15 21:49:29 -0300

    Constrói a build com expo

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import styles from './styles.js'
+
+export default function Main() {
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
